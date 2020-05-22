410e60ccf937f2033662b0a09b50ff4a366a4701
cxx <1097654727@qq.com>
2020-05-22 10:58:27 +0800

    第二次

--- demo1.py
+++ demo1.py
@@ -9,3 +9,13 @@ print(a)
 b=(input("请输入"))
 c=(input("请输入"))
 print("两端长度是",len(b)+len(c))
+print(False)
+print(2>3)
+print(2<3)
+a="张三"
+print(a)
+b=(input("请输入"))
+c=(input("请输入"))
+print("两端长度是",len(b)+len(c))
+
+
